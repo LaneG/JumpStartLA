d485388b6aae2f49fcd6294190b3da0335660b57
Keith Merrill IV <keith@gophilosophie.com>
2015-03-01 15:11:14 -0800

    amend Apple HK query extensions with method for obtaining number of steps taken since midnight today

--- MoveIt/HKHealthStore+AAPLExtensions.m
+++ MoveIt/HKHealthStore+AAPLExtensions.m
@@ -37,4 +37,59 @@
     [self executeQuery:query];
 }
 
+- (void)appl_getStepCountForPast24HoursWithCompletion:(void (^)(NSArray *, NSError *))completion
+{
+    NSDate *startDate, *endDate, *anchorDate;
+    
+    startDate = anchorDate = [self dateAtBeginningOfDayForDate:[NSDate date]];
+    endDate = [NSDate date];
+    
+    HKQuantityType *stepsCount = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+    NSPredicate *predicate = [HKQuery predicateForSamplesWithStartDate:startDate endDate:endDate options:HKQueryOptionStrictStartDate];
+    
+    // Your interval: sum by hour
+    NSDateComponents *intervalComponents = [[NSDateComponents alloc] init];
+    intervalComponents.hour = 1;
+    
+    HKStatisticsCollectionQuery *query = [[HKStatisticsCollectionQuery alloc] initWithQuantityType:stepsCount quantitySamplePredicate:predicate options:HKStatisticsOptionCumulativeSum anchorDate:anchorDate intervalComponents:intervalComponents];
+    query.initialResultsHandler = ^(HKStatisticsCollectionQuery *query, HKStatisticsCollection *result, NSError *error) {
+        if (!result) {
+            if (completion) {
+                completion(nil, error);
+            }
+            
+            return;
+        }
+        
+        if (completion) {
+            HKStatisticsCollection *quantitySample = result;
+            NSArray *quantity = quantitySample.statistics;
+            
+            completion(quantity, error);
+        }
+    };
+    
+    [self executeQuery:query];
+}
+
+- (NSDate *)dateAtBeginningOfDayForDate:(NSDate *)inputDate
+{
+    // Use the user's current calendar and time zone
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSTimeZone *timeZone = [NSTimeZone systemTimeZone];
+    [calendar setTimeZone:timeZone];
+    
+    // Selectively convert the date components (year, month, day) of the input date
+    NSDateComponents *dateComps = [calendar components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay fromDate:inputDate];
+    
+    // Set the time components manually
+    [dateComps setHour:0];
+    [dateComps setMinute:0];
+    [dateComps setSecond:0];
+    
+    // Convert back
+    NSDate *beginningOfDay = [calendar dateFromComponents:dateComps];
+    return beginningOfDay;
+}
+
 @end
